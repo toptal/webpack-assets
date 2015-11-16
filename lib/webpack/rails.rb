5ddbccf58b3122c476c7ac48e38f735147853876
Sergey Nartimov <just.lest@gmail.com>
2015-11-16 19:15:39 +0300

    Set default configuration in before_initialize

--- lib/webpack/rails.rb
+++ lib/webpack/rails.rb
@@ -3,7 +3,7 @@ require 'rails/railtie'
 module Webpack
   module Assets
     class Railtie < Rails::Railtie
-      config.after_initialize do
+      config.before_initialize do
         Webpack.config.use_server  =  Rails.env.development?
         Webpack.config.extract_css = !Rails.env.development?
       end
